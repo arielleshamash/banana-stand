92cef87740319ae18f1086b86cc5e2cc59b361b6
Arisham <arielleshamash@gmail.com>
2022-01-26 17:28:24 -0800

    update gitignore

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Head from "./components/HeadSection";
 import TopBlurb from "./components/TopBlurb";
 import WaitWhat from "./components/WaitWhat";
 import Peggy from "./components/Peggy";
+import Slideshow from "./components/Pictures";
 
 function App() {
   return (
     <>
       <Head />
       <TopBlurb />
+      <Slideshow />
       <WaitWhat />
       <Peggy />
       <FAQsection />
