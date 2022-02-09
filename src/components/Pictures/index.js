c9f569e7c6ef67d920942004fb73f06fc19f91b4
Arisham <arielleshamash@gmail.com>
2022-02-08 16:41:42 -0800

    add framed pics

--- src/components/Pictures/index.js
+++ src/components/Pictures/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./styles/index.css";
-import { Container } from "@material-ui/core";
-import grid from "../HeadSection/images/gridv2.png";
+import { Container, Grid } from "@material-ui/core";
+import photoGrid from "../HeadSection/images/gridv2.png";
+import Framed1 from "../HeadSection/images/Framed1.png";
+import Framed2 from "../HeadSection/images/Framed2.png";
+import Framed3 from "../HeadSection/images/Framed3.png";
 
 function PictureGrid() {
   return (
@@ -10,16 +13,26 @@ function PictureGrid() {
     "
       style={{ marginBottom: "50px" }}
     >
-      <Container fixed>
+      <Container maxWidth="lg">
         <img
-          src={grid}
+          src={photoGrid}
           alt="banana-pics"
           style={{
-            width: "100%",
-            verticalAlign: "middle",
+            maxWidth: "100%",
             marginBottom: "20px",
           }}
         />
+        <Grid container direction="row">
+          <Grid item xs={4}>
+            <img src={Framed1} alt="framed1" style={{ maxWidth: "90%" }} />
+          </Grid>
+          <Grid item xs={4}>
+            <img src={Framed2} alt="framed2" style={{ maxWidth: "90%" }} />
+          </Grid>
+          <Grid item xs={4}>
+            <img src={Framed3} alt="framed3" style={{ maxWidth: "90%" }} />
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
